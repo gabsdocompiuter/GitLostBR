bbd3dafd1e413c59d9a0b98405d51e2c9cabd7dd
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-07-09 22:37:09 -0300

    Create bot to search tweets

---
+++ src/twitter/twSearch.js
@@ -0,0 +1,26 @@
+const tw = require('./tw.js');
+
+async function bot(status){
+    let twitter = tw();
+    let query = `"${status}" (from:gitlostbr)`;
+
+    return await search(query);
+       
+    async function search(query){
+        return new Promise((resolve) => {
+            twitter.get('search/tweets', {
+                q: query,
+                count: 1
+            }, (err, data, response) => {
+                if(data.statuses.length > 0){
+                    resolve(data.statuses[0].text);
+                }
+                else{
+                    resolve(null);
+                }
+            })
+        });
+    }
+}
+
+module.exports = bot;
