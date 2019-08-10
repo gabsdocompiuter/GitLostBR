ba0450a5c4abdc377d11549c82011f9296d97907
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-08-10 03:41:24 -0300

    Use the new structure of bots

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
-const fs = require('fs');
 const ghBot = require('./bots/GithubBot');
-
-// const twTweet = require('./bots/twitter/twTweet');
-// const twSearch = require('./bots/twitter/twSearch');
+const twBot = require('./bots/TwitterBot');
 
 async function start(){
-    const commits = await ghBot.getCommits()
+    const commits = await getCommits();
+    const messagesToTweet = await checkTweets(commits);
 
-    commits.map((commit) => {
-        console.log(commit);
-        console.log();
-    })
+    await tweetCommits(messagesToTweet);
 }
 
-async function readCommits(){
-    // githubBot(message => {
-    //     twSearch(message)
-    //         .then(resultSearch => {
-    //             if(resultSearch === null){
-    //                 console.log(`>> Tweeting commit... '${message}'`);
-    //                 // twTweet(message);
-    //             }
-    //             else{
-    //                 console.log(`>> Commit already tweeted: '${message}'`);
-    //             }
-    //         });    
-    // });
+async function waitEach(array, callback, time){
+    const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+
+    for(let i = 0; i < array.length; i++){
+        await waitFor(time);
+        await callback(array[i], i, array);
+    }
+}
+
+async function getCommits(){
+    console.log(`>>>> Buscando commits do GitHub...`);
+    console.log();
+
+    return await ghBot.getCommits()
+}
+
+async function checkTweets(commits){
+    console.log();
+    console.log(`>>>> Verificando commits ainda não twitados...`);
+    console.log();
+
+    const messagesToTweet = [];
+    await waitEach(commits, async (currentCommit) => {
+        const alreadyTwiteed = await twBot.search(currentCommit);
+
+        if(!alreadyTwiteed){
+            messagesToTweet.push(currentCommit);
+        }
+    }, 5000);
+
+    return messagesToTweet;
+}
+
+async function tweetCommits(messagesToTweet){
+    await waitEach(messagesToTweet, async (currentMessage) => {
+        await twBot.tweet(currentMessage);
+    }, 5000);
 }
 
 start();
