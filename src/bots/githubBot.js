70afc6f4738362de8a3f059cc211ee2a96ea5bfc
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-08-09 21:35:22 -0300

    Move bots to a src/bots/

---
+++ src/bots/githubBot.js
@@ -0,0 +1,67 @@
+const fetch = require("node-fetch");
+const wordsFilter = require('./json/filterWords.json');
+
+async function bot(callback){
+    console.log(`>> Getting commits...`);
+
+    let requestInfo = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/vnd.github.cloak-preview',
+        }
+    };
+
+    numberOfWords = wordsFilter.words.length;
+
+    await delayedLoop(numberOfWords, 5000, (word) => {
+
+        getLog(word, response => {
+            if(response.total_count != null && response.total_count > 0){
+                response.items.map(item => {
+                    callback(item.commit.message);
+                });
+            }
+        });
+    });
+
+    async function delayedLoop(times, waitTime, callbackFunction){
+        let i = 0;
+
+        loop();
+
+        function loop(){
+            setTimeout(() =>{
+                callbackFunction(wordsFilter.words[i]);
+
+                i++;
+                if(i < times){
+                    loop();
+                }
+            }, waitTime);
+        }
+    }
+
+    async function getLog(word, callback){
+        let url = `https://api.github.com/search/commits?q=${word}+author-date:>=${getDate()}`;
+        fetch(url, requestInfo)
+            .then(r => r.json())
+            .then(json => callback(json))
+            .catch(err => console.error(err));
+    }
+}
+
+function getDate(){
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = leftZeros(d.getMonth() + 1);
+    const day = leftZeros(d.getDate());
+
+    return `${year}-${month}-${day}`;
+
+    function leftZeros(number){
+        const length = number.toString().length;
+        return length == 1 ? `0${number}` : number;
+    }
+}
+
+module.exports = bot;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 const fs = require('fs');
-const githubBot = require('./githubBot');
+const ghBot = require('./bots/githubBot');
 
-const twTweet = require('./twitter/twTweet');
-const twSearch = require('./twitter/twSearch');
+// const twTweet = require('./bots/twitter/twTweet');
+// const twSearch = require('./bots/twitter/twSearch');
 
 async function start(){
-    await readCommits();
-    await setInterval(readCommits, 60000);    
+    await ghBot.getCommits()
+    // await readCommits();
+    // await setInterval(readCommits, 60000);    
 }
 
 async function readCommits(){
-    githubBot(message => {
-        twSearch(message)
-            .then(resultSearch => {
-                if(resultSearch === null){
-                    console.log(`>> Tweeting commit... '${message}'`);
-                    twTweet(message);
-                }
-                else{
-                    console.log(`>> Commit already tweeted: '${message}'`);
-                }
-            });    
-    });
+    // githubBot(message => {
+    //     twSearch(message)
+    //         .then(resultSearch => {
+    //             if(resultSearch === null){
+    //                 console.log(`>> Tweeting commit... '${message}'`);
+    //                 // twTweet(message);
+    //             }
+    //             else{
+    //                 console.log(`>> Commit already tweeted: '${message}'`);
+    //             }
+    //         });    
+    // });
 }
 
 start();
