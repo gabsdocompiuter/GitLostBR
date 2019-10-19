531d3bc885b01c0c27faaa521bdd33941a3af740
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-10-19 18:57:52 -0300

    Ajust variables

--- src/services/TwitterAPI.js
+++ src/services/TwitterAPI.js
@@ -2,7 +2,7 @@ const Twit = require('twit')
 
 module.exports = new Twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
