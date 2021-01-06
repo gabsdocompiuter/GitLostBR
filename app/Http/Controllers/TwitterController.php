3163cd8052c793e3c6a74d176ee652aca4e4079a
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-01-06 17:30:21 -0300

    Add thujohn/twitter

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Http\Controllers;
 
+use Twitter;
+
 class TwitterController extends Controller
 {
     public function tweet()
     {
-        return response()->json(['message' => 'sucesso'], 200);
+        return Twitter::postTweet(['status' => 'Postando pelo laravel', 'format' => 'json']);
     }
 }
