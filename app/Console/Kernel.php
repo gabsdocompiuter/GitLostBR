543cf0338419f2abe6ade8139e5cd9575deacb03
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-01-08 20:59:44 -0300

    Change cron to everyFifteenMinutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('bot:cron')->hourly();
+        $schedule->command('bot:cron')->everyFifteenMinutes();
     }
 
     /**
