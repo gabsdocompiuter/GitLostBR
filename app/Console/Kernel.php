d34958ca5482de86a1b6f08ac32d1b8941568cd4
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-01-08 21:30:36 -0300

    Create run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('bot:cron')->everyFifteenMinutes();
+        $schedule->command('bot:cron')->everyMinute();
     }
 
     /**
